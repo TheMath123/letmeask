e98c9641c27405cf8749480da533b246082adf76
Matheus P. Agostinho <mat.matheus_pa@hotmail.com>
2021-06-22 14:20:35 -0300

    autentificação com o google

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Home } from "./pages/Home";
+import { NewRoom } from './pages/NewRoom';
+
+import './styles/global.scss';
+
 export default function App() {
   return (
-    <div>Hello world!</div>
+    <BrowserRouter>
+      <Route path='/' exact component={Home}/>
+      <Route path='/rooms/new' component={NewRoom}/>
+    </BrowserRouter>
   );
 }
